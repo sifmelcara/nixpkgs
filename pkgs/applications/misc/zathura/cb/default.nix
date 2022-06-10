6203fa7badf423ab7cf782466a1637b93e3e80e1
mingchuan <ming@culpring.com>
2022-06-10 20:35:36 +0800

    Update zathura-cb to latest version

--- pkgs/applications/misc/zathura/cb/default.nix
+++ pkgs/applications/misc/zathura/cb/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "zathura-cb";
-  version = "0.1.8";
+  version = "cdb306d2aaaab0825f3bcddef3863cd708fe6c95";
 
   src = fetchurl {
-    url = "https://pwmt.org/projects/${pname}/download/${pname}-${version}.tar.xz";
-    sha256 = "1i6cf0vks501cggwvfsl6qb7mdaf3sszdymphimfvnspw810faj5";
+    url = "https://git.pwmt.org/pwmt/zathura-cb/-/archive/cdb306d2aaaab0825f3bcddef3863cd708fe6c95/zathura-cb-cdb306d2aaaab0825f3bcddef3863cd708fe6c95.tar";
+    sha256 = "sha256-vcCsyIogTm/2IuxUAOzU3h1C8M0YuGiRDz+0r3tyfXw=";
   };
 
   nativeBuildInputs = [ meson ninja pkg-config gettext ];
