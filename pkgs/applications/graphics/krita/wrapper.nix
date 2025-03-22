0ba250dabce301fedeab72482464e890c8bb2e92
unknown <unknown>
2025-03-22 16:39:57 +0800

    Unwrap krita derivation
    Make nix dev shell use krita derivation directly

--- pkgs/applications/graphics/krita/wrapper.nix
+++ pkgs/applications/graphics/krita/wrapper.nix
@@ -10,24 +10,4 @@
   ],
 }:
 
-symlinkJoin {
-  name = lib.replaceStrings [ "-unwrapped" ] [ "" ] unwrapped.name;
-  inherit (unwrapped)
-    version
-    buildInputs
-    nativeBuildInputs
-    meta
-    ;
-
-  paths = [ unwrapped ] ++ binaryPlugins;
-
-  postBuild = ''
-    wrapQtApp "$out/bin/krita" \
-      --prefix PYTHONPATH : "$PYTHONPATH" \
-      --set KRITA_PLUGIN_PATH "$out/lib/kritaplugins"
-  '';
-
-  passthru = {
-    inherit unwrapped binaryPlugins;
-  };
-}
+unwrapped
