198373afd25e219e0f84ad4faf8c1d4cf3e194bd
Ming-Chuan <ming@culpring.com>
2026-02-26 00:42:44 +0800

    lager: add maintainer

--- pkgs/by-name/la/lager/package.nix
+++ pkgs/by-name/la/lager/package.nix
@@ -37,6 +37,6 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://github.com/arximboldi/lager";
     description = "C++ library for value-oriented design using the unidirectional data-flow architecture — Redux for C++";
     license = lib.licenses.mit;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ sifmelcara ];
   };
 })
